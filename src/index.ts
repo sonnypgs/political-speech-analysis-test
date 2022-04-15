69d605794f5316a89bc3417017dec522e7025a19
Sonny Schmidt <pgs.sonny@gmail.com>
2022-04-15 13:58:27 +0800

    init: add express app

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-import { sayHello } from '~/lib/utils';
+import express, { Request, Response, Express } from 'express';
 
-sayHello('sonny');
+const app: Express = express();
+const PORT = 3003;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('just testing');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
